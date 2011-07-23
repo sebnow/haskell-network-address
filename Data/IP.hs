c84e640c0a6dd16fecf130e9398ef221817dbafc
Sebastian Nowicki <sebnow@gmail.com>
2011-07-23 23:48:47 +0800

    Switch from Parsec to plain parser
    Replacing Parsec with span for parsing IP addresses improved performance
    by almost 4x, from ~17us to ~5us on an Intel Atom 330 1.6GHz machine.

--- Data/IP.hs
+++ Data/IP.hs
@@ -8,10 +8,9 @@ import Control.Monad (when)
 import Data.Bits (shift)
 import Data.Char (digitToInt)
 import Data.List (foldl')
+import Data.Char (digitToInt, isDigit)
+import Data.List (foldl')
 import Data.Word
-import qualified Text.Parsec as P
-import qualified Text.Parsec.String as P
-import Text.Parsec.Prim (unexpected)
 import Text.Printf (printf)
 
 -- |The abstract data structure to represent an IPv4 address.
@@ -22,9 +21,12 @@ instance Show IPv4 where
     show = showIPv4
 
 instance Read IPv4 where
-    readsPrec _ s = case P.parse (toReadS ipv4parser) "ipv4" s of
-        Left _  -> error "ipv4"
-        Right x -> x 
+    readsPrec _ s = [(IPv4 $ a' `shift` 24 + b' `shift` 16 + c' `shift` 8 + d', s')]
+        where (a, _:xs1)       = span isDigit s
+              (b, _:xs2)       = span isDigit xs1
+              (c, _:xs3)       = span isDigit xs2
+              (d, s' )         = span isDigit xs3
+              [a', b', c', d'] = map digitsToWord32 [a, b, c, d]
 
 -- |Return the byte representation of an IPv4 IP address.
 toIPv4 :: Word32 -> IPv4
@@ -42,25 +44,8 @@ showIPv4 (IPv4 ip) = printf "%d.%d.%d.%d" a b c d
           (r1, b) = shift8 r2
           (r2, c) = shift8 r3
           (r3, d) = shift8 ip
-          shift8 = (`divMod` 256)
+          shift8  = (`divMod` 256)
 
-digitsToWord8 :: [Char] -> Word8
-digitsToWord8 = foldl' (\x y -> x * 10 + y) 0 . map (fromIntegral . digitToInt)
-
-ipv4digit :: P.Parser Word8
-ipv4digit = fmap digitsToWord8 (P.many1 P.digit)
-
-ipv4parser :: P.Parser IPv4
-ipv4parser = do
-    ds <- ipv4digit `P.sepBy1` (P.char '.')
-    when (length ds /= 4) (unexpected "IPv4")
-    mapM_ (\x -> when (x < 0 || x > 255) (unexpected "IPv4")) ds
-    let [a, b, c, d] = map fromIntegral ds :: [Word32]
-    return . IPv4 $ a `shift` 24 + b `shift` 16 + c `shift` 8 + d
-
-toReadS :: P.Parser a -> P.Parser [(a, String)]
-toReadS p = do
-    x <- p
-    r <- P.getInput
-    return [(x, r)]
+digitsToWord32 :: [Char] -> Word32
+digitsToWord32 = foldl' (\x y -> x * 10 + y) 0 . map (fromIntegral . digitToInt)
 
