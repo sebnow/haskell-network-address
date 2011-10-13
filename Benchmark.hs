b56a4d1d5c93dcbaf5d6ffe7e29d10c73ac43868
Sebastian Nowicki <sebnow@gmail.com>
2011-10-13 21:50:26 +0800

    Use normal form instead of WHNF for benchmarks

--- Benchmark.hs
+++ Benchmark.hs
@@ -1,12 +1,21 @@
 module Main where
+import Control.DeepSeq (NFData, rnf)
 import Criterion.Main
 import Data.Network.Address
+
+instance NFData IPv4 where
+    rnf (IPv4 ip) = rnf ip
+
+instance NFData IPv6 where
+    rnf (IPv6 a b) = rnf a `seq` rnf b
+
+instance (Address a, NFData a) => NFData (IPSubnet a) where
+    rnf (IPSubnet ip m) = rnf ip `seq` rnf m
 
 ipv4str :: Integral a => a -> String
 ipv4str n = "192.168." ++ show (n `div` 255)
                        ++ "."
                        ++ show (n `mod` 255)
-
 
 ipv4 :: IPv4
 ipv4 = toAddress 3232235777
@@ -22,21 +31,21 @@ ipv6subnet = IPSubnet ipv6 (toMask (56 :: Integer))
 
 main = defaultMain
     [ bgroup "IPv4"
-        [ bench "readAddress" $ whnf (readAddress :: String -> IPv4) "192.168.1.1"
-        , bench "showAddress" $ whnf showAddress ipv4
+        [ bench "readAddress" $ nf (readAddress :: String -> IPv4) "192.168.1.1"
+        , bench "showAddress" $ nf showAddress ipv4
         ]
     , bgroup "IPv6"
-        [ bench "readAddress" $ whnf (readAddress :: String -> IPv6) "2001:0db8:85a3:0:0:8a2e:0370:7334"
-        , bench "showAddress" $ whnf showAddress ipv6
+        [ bench "readAddress" $ nf (readAddress :: String -> IPv6) "2001:0db8:85a3:0:0:8a2e:0370:7334"
+        , bench "showAddress" $ nf showAddress ipv6
         ]
     , bgroup "Subnet"
         [ bgroup "IPv4"
-            [ bench "readSubnet" $ whnf (readSubnet :: String -> IPSubnet IPv4) "192.168.1.10/8"
-            , bench "showSubnet" $ whnf showSubnet ipv4subnet
+            [ bench "readSubnet" $ nf (readSubnet :: String -> IPSubnet IPv4) "192.168.1.10/8"
+            , bench "showSubnet" $ nf showSubnet ipv4subnet
             ]
         , bgroup "IPv6"
-            [ bench "readSubnet" $ whnf (readSubnet :: String -> IPSubnet IPv6) "2001:0db8:85a3:0:0:8a2e:0370:7334/56"
-            , bench "showSubnet" $ whnf showSubnet ipv6subnet
+            [ bench "readSubnet" $ nf (readSubnet :: String -> IPSubnet IPv6) "2001:0db8:85a3:0:0:8a2e:0370:7334/56"
+            , bench "showSubnet" $ nf showSubnet ipv6subnet
             ]
         ]
     ]
