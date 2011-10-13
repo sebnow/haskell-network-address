0224a8c12a5e33dae4e492a70a71ed97b27cfb3b
Sebastian Nowicki <sebnow@gmail.com>
2011-10-13 21:33:20 +0800

    Reorganize benchmarks

--- Benchmark.hs
+++ Benchmark.hs
@@ -22,29 +22,21 @@ ipv6subnet = IPSubnet ipv6 (toMask (56 :: Integer))
 
 main = defaultMain
     [ bgroup "IPv4"
-        [ bgroup "Parse"
-            [ bench "read" $ whnf (readAddress :: String -> IPv4) "192.168.1.1"
-            ]
-        , bgroup "PrettyPrint"
-            [ bench "show" $ whnf showAddress ipv4
-            ]
+        [ bench "readAddress" $ whnf (readAddress :: String -> IPv4) "192.168.1.1"
+        , bench "showAddress" $ whnf showAddress ipv4
         ]
     , bgroup "IPv6"
-        [ bgroup "Parse"
-            [ bench "read" $ whnf (readAddress :: String -> IPv6) "2001:0db8:85a3:0:0:8a2e:0370:7334"
-            ]
-        , bgroup "PrettyPrint"
-            [ bench "show" $ whnf showAddress ipv6
-            ]
+        [ bench "readAddress" $ whnf (readAddress :: String -> IPv6) "2001:0db8:85a3:0:0:8a2e:0370:7334"
+        , bench "showAddress" $ whnf showAddress ipv6
         ]
     , bgroup "Subnet"
-        [ bgroup "Parse"
-            [ bench "readIPv4" $ whnf (readSubnet :: String -> IPSubnet IPv4) "192.168.1.10/8"
-            , bench "readIPv6" $ whnf (readSubnet :: String -> IPSubnet IPv6) "2001:0db8:85a3:0:0:8a2e:0370:7334/56"
+        [ bgroup "IPv4"
+            [ bench "readSubnet" $ whnf (readSubnet :: String -> IPSubnet IPv4) "192.168.1.10/8"
+            , bench "showSubnet" $ whnf showSubnet ipv4subnet
             ]
-        , bgroup "PrettyPrint"
-            [ bench "showIPv4" $ whnf showSubnet ipv4subnet
-            , bench "showIPv6" $ whnf showSubnet ipv6subnet
+        , bgroup "IPv6"
+            [ bench "readSubnet" $ whnf (readSubnet :: String -> IPSubnet IPv6) "2001:0db8:85a3:0:0:8a2e:0370:7334/56"
+            , bench "showSubnet" $ whnf showSubnet ipv6subnet
             ]
         ]
     ]
